0592cb6c1e97f84958f551ae3d01b03d7a0abbab
Shekhar Bankapur <shekhar.bankapur@capgemini.com>
2021-12-15 21:03:56 +0530

    c

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,41 @@
 import { render, screen } from '@testing-library/react';
-// import { act } from 'react-dom/test-utils';
 import Hello from './components/Hello';
 import EmpData from './components/EmpData';
 import SpringBootData from './components/SpringBootData';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
-
 // Website https://jestjs.io/ 
 // learning resources https://jestjs.io/docs/getting-started  
 //api https://jestjs.io/docs/api  
 
+// run - 
+// npm start 
+// afterwards 
+// Watch Usage
+//  › Press a to run all tests.
+//  › Press f to run only failed tests.
+//  › Press q to quit watch mode.
+//  › Press p to filter by a filename regex pattern.
+//  › Press t to filter by a test name regex pattern.
+//  › Press Enter to trigger a test run.
+
+// sytnax to write test cases 
+// test('a string value that describes this test', () => { });
+
+// beforeAll();
+// beforeEach();
+// afterAll();
+// afterEach();
+
+// use this hook to render a component only once before all the test cases 
+beforeAll(() => {
+  console.log('beforeAll');
+});
+
+beforeEach(() => {
+  console.log('beforreEach');
+});
 
 // positive test case 
 test('render Data from Hello', () => {
@@ -29,6 +54,7 @@ test('render Data from EmpData', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+
 // positive test case 
 test('render Data from SpringBootData', () => {
   render(
@@ -39,16 +65,17 @@ test('render Data from SpringBootData', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+// negative test case 
+test('render Data from SpringBootData', () => {
+  render(
+    <Provider store={store} >
+      <SpringBootData />
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
 
-// // negative test case 
-// test('not to render wrong data', () => {
-//   render(<Hello />);
-//   const linkElement = screen.getByText(/Capgemini is a good company./);
-//   expect(linkElement).not.toBeInTheDocument();
-// });
 
-
-// test('a string value that describes this test', () => { });
 
 // import { render, screen } from '@testing-library/react';
 // import App from './App';
